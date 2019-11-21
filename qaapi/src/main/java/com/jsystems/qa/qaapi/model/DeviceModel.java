66826c85be8600ee165f0fbd3bb8b58530fd32be
jagoda89 <jagodaszybiak@gmail.com>
2019-11-21 16:19:03 +0100

    add to gitignore iml files

---
+++ qaapi/src/main/java/com/jsystems/qa/qaapi/model/DeviceModel.java
@@ -0,0 +1,13 @@
+package com.jsystems.qa.qaapi.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class DeviceModel {
+
+    @JsonProperty(required =  true)
+    public String produce;
+
+    @JsonProperty(value = "screen.size", required =  true)
+    public double screenSize;
+
+}
